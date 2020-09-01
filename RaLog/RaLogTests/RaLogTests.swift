41e17eaa5e793ed2c325112603ebadc279928194
Rakuyo <rakuyo.mo@gmail.com>
2020-09-01 16:45:51 +0800

    Adjust the project file while removing the pod

--- RaLog/RaLogTests/RaLogTests.swift
+++ RaLog/RaLogTests/RaLogTests.swift
@@ -1,6 +1,6 @@
 //
-//  Tests.swift
-//  RaLog
+//  RaLogTests.swift
+//  RaLogTests
 //
 //  Created by Rakuyo on 2020/09/01.
 //  Copyright © 2020 Rakuyo. All rights reserved.
@@ -9,22 +9,22 @@
 import XCTest
 @testable import RaLog
 
-class Tests: XCTestCase {
-
-    override func setUp() {
+class RaLogTests: XCTestCase {
+    
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
-    override func tearDown() {
+    
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
+    
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
+    
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
