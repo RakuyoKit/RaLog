55a8d3d430b4db0e8666203f570aaf1d7930de2c
Rakuyo <rakuyo.mo@gmail.com>
2020-09-01 17:04:32 +0800

    Improve UI details

--- RaLog/RaLog/Other/ViewController.swift
+++ RaLog/RaLog/Other/ViewController.swift
@@ -16,5 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { .default }
 }
 
