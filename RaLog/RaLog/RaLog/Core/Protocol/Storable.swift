1374b6c5fb908bd9c6fca59dd09f7c204d42abb6
Rakuyo <rakuyo.mo@gmail.com>
2020-09-01 11:09:00 +0800

    Update notes

--- RaLog/RaLog/RaLog/Core/Protocol/Storable.swift
+++ RaLog/RaLog/RaLog/Core/Protocol/Storable.swift
@@ -31,7 +31,7 @@ public struct StorageMode: OptionSet {
 
 // MARK: - Protocol
 
-/// Used to store logs
+/// Provide the ability to store logs
 public protocol Storable {
     
     /// Storage mode, the default is `.all`. Namely disk cache and memory cache
