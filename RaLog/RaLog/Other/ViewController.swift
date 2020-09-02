742dd093d5b8077a8746c27e570940711cd29f40
Rakuyo <rakuyo.mo@gmail.com>
2020-09-02 11:18:40 +0800

    Add Test

--- RaLog/RaLog/Other/ViewController.swift
+++ RaLog/RaLog/Other/ViewController.swift
@@ -9,14 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Log.appear(self)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        Log.disappear(self)
+    }
+    
+    deinit {
+        Log.deinit(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        Log.debug("Note the output of the console")
         
+        let needSuccess = true
+        
+        Log.warning("Please note that the request is about to start！😎")
+        
+        // Analog api request
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            
+            if needSuccess {
+                Log.success("request success 🥳, content: api request succeeded")
+                
+            } else {
+                Log.error("request failure 😢, error: \("some error")")
+            }
+        }
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle { .default }
 }
-
