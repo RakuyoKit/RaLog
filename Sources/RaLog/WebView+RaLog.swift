4116aaf3f7c84096232e2365fa3dc1ffbbb369d8
Rakuyo <rakuyo.mo@gmail.com>
2020-11-23 11:38:06 +0800

    Adjusting the namespace of extended methods.

--- Sources/RaLog/WebView+RaLog.swift
+++ Sources/RaLog/WebView+RaLog.swift
@@ -8,7 +8,28 @@
 
 import WebKit
 
-extension WKWebView: ExtendableCompatible {
+/// Wrapper for RaLog compatible types.
+/// This type provides an extension point for connivence methods in RaLog.
+public struct RaLogWrapper<Base> {
+    
+    public var base: Base
+    public init(_ base: Base) { self.base = base }
+}
+
+/// Represents an any type that is compatible with RaLog.
+/// You can use `rl` property to get a value in the namespace of RaLog.
+public protocol RaLogCompatible { }
+
+public extension RaLogCompatible {
+    
+    /// Gets a namespace holder for Kingfisher compatible types.
+    var rl: RaLogWrapper<Self> {
+        get { return RaLogWrapper(self) }
+        set { }
+    }
+}
+
+extension WKWebView: RaLogCompatible {
     
     /// Add script to WKWebView for intercepting `console.log`.
     ///
