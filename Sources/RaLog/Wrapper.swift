90c493fab6255bb097030c8cdd7d5ea2c1628695
Rakuyo <rakuyo.mo@gmail.com>
2021-09-02 09:40:18 +0800

    add Atomic Property Wrapper

--- Sources/RaLog/Wrapper.swift
+++ Sources/RaLog/Wrapper.swift
@@ -25,3 +25,30 @@ class Wrapper {
         cacheDateFormatter.dateFormat = "yyyy_MM_dd"
     }
 }
+
+@propertyWrapper
+struct Atomic<Value> {
+    private var value: Value
+    private let lock = NSLock()
+
+    init(wrappedValue value: Value) {
+        self.value = value
+    }
+    
+    var wrappedValue: Value {
+      get { return load() }
+      set { store(newValue: newValue) }
+    }
+    
+    func load() -> Value {
+        lock.lock()
+        defer { lock.unlock() }
+        return value
+    }
+    
+    mutating func store(newValue: Value) {
+        lock.lock()
+        defer { lock.unlock() }
+        value = newValue
+    }
+}
