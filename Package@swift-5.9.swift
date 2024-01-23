66c78280d1187a426e6b7887dc558731d366ff2f
Rakuyo <rakuyo.mo@gmail.com>
2024-01-23 13:04:15 +0800

    [Update] support visionOS

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "RaLog",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_14),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(name: "RaLog", targets: ["RaLog"])
+    ],
+    targets: [
+        .target(
+            name: "RaLog",
+            path: "Sources"
+        )
+    ]
+)
